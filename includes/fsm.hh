39e4b0440b592490e9c201b6e9f3821de07e09d9
Vishal Devadiga <vishaldevadiga003@gmail.com>
2023-11-16 15:21:14 +0530

    Add Files

---
+++ includes/fsm.hh
@@ -0,0 +1,7 @@
+#pragma once
+#include "lang_headers/dfa.hh"
+#include "lang_headers/nfa.hh"
+#include "lang_headers/pda.hh"
+#include "lang_headers/dfa.hh"
+#include "lang_headers/cfg.hh"
+#include "lang_headers/reg.hh"
