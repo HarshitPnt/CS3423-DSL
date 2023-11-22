3e94a2ff1dee783bf631521831429606c7688217
Harshit Pant <cs21btech11021@iith.ac.in>
2023-11-22 15:29:37 +0530

    sets implementation

--- includes/lang_headers/set.hh
+++ includes/lang_headers/set.hh
@@ -9,173 +9,179 @@
 #include <vector>
 #include <iostream>
 
-template <typename T>
-class o_set
+namespace fsm
 {
-public:
-    std::set<T> set;
-    int size();
-    bool find(T x);
-    void insert(T x);
-    void remove(T x);
-    bool empty();
-    void del();
-    void out();
-    ~o_set();
-    o_set();
-    o_set(std::vector<T>);
-};
+    template <typename T>
+    class o_set
+    {
+    public:
+        std::set<T> set;
+        int size();
+        bool find(T x);
+        void insert(T x);
+        void remove(T x);
+        bool empty();
+        void del();
+        void out();
+        ~o_set();
+        o_set();
+        o_set(std::vector<T>);
+    };
 
-template <typename T>
-class u_set
-{
-public:
-    std::unordered_set<T> set;
-    int size();
-    bool find(T x);
-    void insert(T x);
-    void remove(T x);
-    bool empty();
-    void del();
-    void out();
-    ~u_set();
-    u_set();
-    u_set(std::vector<T>);
-};
+    template <typename T>
+    class u_set
+    {
+    public:
+        std::unordered_set<T> set;
+        int size();
+        bool find(T x);
+        void insert(T x);
+        void remove(T x);
+        bool empty();
+        void del();
+        void out();
+        ~u_set();
+        u_set();
+        u_set(std::vector<T>);
+    };
 
-template <typename T>
-o_set<T>::o_set()
-{
-    this->set = std::set<T>();
-}
+    template <typename T>
+    o_set<T>::o_set()
+    {
+        this->set = std::set<T>();
+    }
 
-template <typename T>
-o_set<T>::o_set(std::vector<T> elements)
-{
-    this->set = std::set<T>();
-    for (auto element : elements)
+    template <typename T>
+    o_set<T>::o_set(std::vector<T> elements)
+    {
+        this->set = std::set<T>();
+        for (auto element : elements)
+        {
+            this->set.insert(element);
+        }
+    }
+
+    template <typename T>
+    int o_set<T>::size()
+    {
+        return this->set.size();
+    }
+
+    template <typename T>
+    bool o_set<T>::find(T element)
+    {
+        return this->set.find(element) != this->set.end();
+    }
+
+    template <typename T>
+    void o_set<T>::insert(T element)
     {
         this->set.insert(element);
     }
-}
 
-template <typename T>
-int o_set<T>::size()
-{
-    return this->set.size();
-}
-
-template <typename T>
-bool o_set<T>::find(T element)
-{
-    return this->set.find(element) != this->set.end();
-}
-
-template <typename T>
-void o_set<T>::insert(T element)
-{
-    this->set.insert(element);
-}
-
-template <typename T>
-void o_set<T>::remove(T element)
-{
-    this->set.erase(element);
-}
-
-template <typename T>
-bool o_set<T>::empty()
-{
-    return this->set.empty();
-}
-
-template <typename T>
-void o_set<T>::out()
-{
-    for (auto element : this->set)
+    template <typename T>
+    void o_set<T>::remove(T element)
     {
-        std::cout << element << " ";
+        this->set.erase(element);
     }
-    std::cout << std::endl;
-}
 
-template <typename T>
-void o_set<T>::del()
-{
-    this->set.clear();
-}
+    template <typename T>
+    bool o_set<T>::empty()
+    {
+        return this->set.empty();
+    }
 
-template <typename T>
-o_set<T>::~o_set()
-{
-    this->set.clear();
-}
+    template <typename T>
+    void o_set<T>::out()
+    {
+        std::cout << "{ ";
+        for (auto element : this->set)
+        {
+            std::cout << element << " ";
+        }
+        std::cout << "}" << std::endl;
+    }
 
-template <typename T>
-u_set<T>::u_set()
-{
-    this->set = std::unordered_set<T>();
-}
+    template <typename T>
+    void o_set<T>::del()
+    {
+        this->set.clear();
+    }
 
-template <typename T>
-u_set<T>::u_set(std::vector<T> elements)
-{
-    this->set = std::unordered_set<T>();
-    for (auto element : elements)
+    template <typename T>
+    o_set<T>::~o_set()
+    {
+        this->set.clear();
+    }
+
+    template <typename T>
+    u_set<T>::u_set()
+    {
+        this->set = std::unordered_set<T>();
+    }
+
+    template <typename T>
+    u_set<T>::u_set(std::vector<T> elements)
+    {
+        this->set = std::unordered_set<T>();
+        for (auto element : elements)
+        {
+            this->set.insert(element);
+        }
+    }
+
+    template <typename T>
+    int u_set<T>::size()
+    {
+        return this->set.size();
+    }
+
+    template <typename T>
+    bool u_set<T>::find(T element)
+    {
+        return this->set.find(element) != this->set.end();
+    }
+
+    template <typename T>
+    void u_set<T>::insert(T element)
     {
         this->set.insert(element);
     }
-}
 
-template <typename T>
-int u_set<T>::size()
-{
-    return this->set.size();
-}
-
-template <typename T>
-bool u_set<T>::find(T element)
-{
-    return this->set.find(element) != this->set.end();
-}
-
-template <typename T>
-void u_set<T>::insert(T element)
-{
-    this->set.insert(element);
-}
-
-template <typename T>
-void u_set<T>::remove(T element)
-{
-    this->set.erase(element);
-}
-
-template <typename T>
-bool u_set<T>::empty()
-{
-    return this->set.empty();
-}
-
-template <typename T>
-void u_set<T>::out()
-{
-    for (auto element : this->set)
+    template <typename T>
+    void u_set<T>::remove(T element)
     {
-        std::cout << element << " ";
+        this->set.erase(element);
     }
-    std::cout << std::endl;
+
+    template <typename T>
+    bool u_set<T>::empty()
+    {
+        return this->set.empty();
+    }
+
+    template <typename T>
+    void u_set<T>::out()
+    {
+        std::cout << "{ ";
+        for (auto element : this->set)
+        {
+            std::cout << element << " ";
+        }
+        std::cout << "}" << std::endl;
+    }
+
+    template <typename T>
+    void u_set<T>::del()
+    {
+        this->set.clear();
+    }
+
+    template <typename T>
+    u_set<T>::~u_set()
+    {
+        this->set.clear();
+    }
 }
 
-template <typename T>
-void u_set<T>::del()
-{
-    this->set.clear();
-}
-
-template <typename T>
-u_set<T>::~u_set()
-{
-    this->set.clear();
-}
 #endif
--- tests/codegen/test_set.cc
+++ tests/codegen/test_set.cc
@@ -2,88 +2,71 @@
 #include <iostream>
 
 using namespace std;
-class test
-{
-    int a;
-};
-
-template <typename T>
-class obj
-{
-public:
-    T x;
-    void display();
-};
-
-template <typename T>
-void obj<T>::display()
-{
-    cout << "Hello World" << endl;
-}
 
 int main()
 {
     // Test for o_set
-    o_set<int> ord_s;
+    fsm::o_set<int> ord_s;
     // Test 1: Insertion
-    ord_s.insert(1);
-    // obj<int> o;
-    // o.display();
+    ord_s.insert(2);
+    ord_s.insert(3);
+    ord_s.insert(4);
+    ord_s.insert(5);
     ord_s.out();
     // Test 2: Removal
-    // ord_s.remove(3);
-    // ord_s.out();
-    // ord_s.del();
-    // ord_s.out();
+    ord_s.remove(3);
+    ord_s.out();
+    ord_s.del();
+    ord_s.out();
 
     // // Test 3: Find
-    // cout << ord_s.find(1) << "\n";
-    // ord_s.insert(1);
-    // cout << ord_s.find(1) << "\n";
+    cout << ord_s.find(1) << "\n";
+    ord_s.insert(1);
+    cout << ord_s.find(1) << "\n";
 
     // // Test 4: Empty
-    // cout << ord_s.empty() << "\n";
-    // ord_s.remove(1);
-    // cout << ord_s.empty() << "\n";
+    cout << ord_s.empty() << "\n";
+    ord_s.remove(1);
+    cout << ord_s.empty() << "\n";
 
     // // Test 5: Size
-    // cout << ord_s.size() << "\n";
-    // ord_s.insert(1);
-    // ord_s.insert(2);
-    // ord_s.insert(6);
-    // cout << ord_s.size() << "\n";
+    cout << ord_s.size() << "\n";
+    ord_s.insert(1);
+    ord_s.insert(2);
+    ord_s.insert(6);
+    cout << ord_s.size() << "\n";
 
     // // Test for u_set
-    // fsm::u_set<int> unord_s;
+    fsm::u_set<int> unord_s;
     // // Test 1: Insertion
-    // unord_s.insert(2);
-    // unord_s.insert(1);
-    // unord_s.insert(4);
-    // unord_s.insert(3);
-    // unord_s.insert(5);
-    // unord_s.out();
+    unord_s.insert(2);
+    unord_s.insert(1);
+    unord_s.insert(4);
+    unord_s.insert(3);
+    unord_s.insert(5);
+    unord_s.out();
     // // Test 2: Removal
-    // unord_s.remove(3);
-    // unord_s.out();
-    // unord_s.del();
-    // unord_s.out();
+    unord_s.remove(3);
+    unord_s.out();
+    unord_s.del();
+    unord_s.out();
 
     // // Test 3: Find
-    // cout << unord_s.find(1) << "\n";
-    // unord_s.insert(1);
-    // cout << unord_s.find(1) << "\n";
+    cout << unord_s.find(1) << "\n";
+    unord_s.insert(1);
+    cout << unord_s.find(1) << "\n";
 
     // // Test 4: Empty
-    // cout << unord_s.empty() << "\n";
-    // unord_s.remove(1);
-    // cout << unord_s.empty() << "\n";
+    cout << unord_s.empty() << "\n";
+    unord_s.remove(1);
+    cout << unord_s.empty() << "\n";
 
     // // Test 5: Size
-    // cout << unord_s.size() << "\n";
-    // unord_s.insert(1);
-    // unord_s.insert(2);
-    // unord_s.insert(6);
-    // cout << unord_s.size() << "\n";
+    cout << unord_s.size() << "\n";
+    unord_s.insert(1);
+    unord_s.insert(2);
+    unord_s.insert(6);
+    cout << unord_s.size() << "\n";
 
     return 0;
 }
